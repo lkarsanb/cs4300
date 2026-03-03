a91d13697fea651b03cac7988da45ae5f8884dd9
Lea <lkarsanb@uccs.edu>
2026-03-03 06:06:48 +0000

    Fixed logo not showing.

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -139,12 +139,10 @@ USE_TZ = True
 # Static images for the design of the website.
 STATIC_URL = "/static/"
 
-if "RENDER" not in os.environ:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
