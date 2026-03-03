11ca7531799b29452a09c0d93f7f7f11ac44ec2c
Lea <lkarsanb@uccs.edu>
2026-03-03 01:46:57 +0000

    Updated settings.py due to deployment fail.

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # When Debug = False, use STATIC_ROOT
-# if not DEBUG:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# # else:
-# #     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# else:
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
