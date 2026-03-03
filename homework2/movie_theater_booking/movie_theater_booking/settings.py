257453c1f26d65a74699b11c4f1bbd640dc8ed67
Lea <lkarsanb@uccs.edu>
2026-03-03 01:29:47 +0000

    Updated settings.py due to deployment fail.

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -77,9 +77,7 @@ WSGI_APPLICATION = 'movie_theater_booking.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/movie_theater_booking',
-        conn_max_age=600
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True
     )
 }
 
