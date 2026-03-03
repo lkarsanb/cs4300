8c3c497d1a721cd7bb9735fbe85095cc5d26b6ec
Lea <lkarsanb@uccs.edu>
2026-03-03 02:23:22 +0000

    Updated settings.py due to deployment fail.

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-5ly(++#b=c-fk8x7pffw)83a4qh@7o$ekp#)o*n%jud4)==^)5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ['*']
