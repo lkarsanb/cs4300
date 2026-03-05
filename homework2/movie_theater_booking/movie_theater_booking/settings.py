f5bb05137fdadc2baeae47c3646d052c86fbae96
Lea <lkarsanb@uccs.edu>
2026-03-05 07:26:26 +0000

    Remove media URL.

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = [
     "app-lkarsanb-21.devedu.io",
